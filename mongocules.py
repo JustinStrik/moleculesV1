918b1ebef7158bcb63bd0fb3adc852922604996f
JustinStri <jstrikowski@ufl.edu>
2023-02-18 02:57:04 -0500

    more functions fr

--- mongocules.py
+++ mongocules.py
@@ -10,15 +10,30 @@ from tkinter import messagebox
 cluster = MongoClient("mongodb+srv://jstrik:strik@moleculev1.w7biaat.mongodb.net/?retryWrites=true&w=majority")
 
 class Status(Enum):
-    Conflicts = 'conflicts'
-    Warning = 'warning'
-    Success = 'success'
+    CONFLICT = 'conflict'
+    WARNING = 'warning'
+    SUCCESS = 'success'
+    ABORTED = 'aborted'
 
 # Connect to the database
 db = cluster["moleculesTestV1"]
 collection = db["molecules"]
+labels = []
 
-def show_identifiers(data):
+
+def push_to_db(molecule, statuses, row):
+    collection.insert_one(molecule[0])
+    index = statuses.index(molecule)  # get the index of the molecule in the statuses list
+    new_molecule = (molecule[0], Status.SUCCESS)  # create a new tuple with the updated status
+    statuses[index] = new_molecule  # replace the old tuple with the new one
+    labels[row].config(bg='green')
+
+def abort_upload(molecule, statuses):
+    index = statuses.index(molecule)  # get the index of the molecule in the statuses list
+    new_molecule = (molecule[0], Status.ABORTED)  # create a new tuple with the updated status
+    statuses[index] = new_molecule  # replace the old tuple with the new one
+
+def show_identifiers(statuses):
     # Create the popup window
     popup = tk.Toplevel()
     popup.title("Identifiers")
@@ -29,37 +44,38 @@ def show_identifiers(data):
 
     # Add a button to close the window
     button_close = tk.Button(popup, text="Close", command=popup.destroy)
-    button_close.grid(row=len(data)+3, column=1, columnspan=2)
-
+    button_close.grid(row=len(statuses)+3, column=1, columnspan=2)
 
     # Display the identifiers in rows
-    for index, molecule in enumerate(data):
-        identifier = f'{molecule["name"]}_{molecule["basis_sets"]}_{molecule["functional"]}'
-        label = tk.Label(popup, text=identifier)
-        label.grid(row=index+2, column=0)
-        button_override = tk.Button(popup, text="Override", command=popup.destroy)
-        button_remove = tk.Button(popup, text="Remove", command=popup.destroy)
-        button_override.grid(row=index+2, column=1)
-        button_remove.grid(row=index+2, column=2)
+    for row, molecule in enumerate(statuses):
+        # Create a Label widget for each column in the row
+        label = tk.Label(popup, text=molecule[0]["identifier"])
+        labels.append(label) # add the label to the list of labels for ease of color changing
+        label.grid(row=row+2, column=0)
+
+        if molecule[1] == Status.CONFLICT: # if the status is conflict, add an override button which will remove the document from the database and add the new one
+            button_override = tk.Button(popup, text="Override", command=push_to_db(molecule, statuses, row))
+            button_remove = tk.Button(popup, text="Remove", command=popup.destroy)
+        if molecule[1] == Status.WARNING: # if the status is warning, add an abort button which will not add the document to the database
+            button_override = tk.Button(popup, text="Abort", command=abort_upload(molecule, statuses))
+            button_remove = tk.Button(popup, text="Remove", command=popup.destroy)
+
+        button_override.grid(row=row+2, column=1)
+        button_remove.grid(row=row+2, column=2)
+
+        # Set the background color based on the status
+        if molecule[1] == Status.CONFLICT:
+            label.config(bg='red')
+        elif molecule[1] == Status.WARNING:
+            label.config(bg='yellow')
+        elif molecule[1] == Status.SUCCESS:
+            label.config(bg='green')
 
 
     # Make the popup window modal
     popup.grab_set()
     popup.geometry("800x600")
     popup.mainloop()
-
-
-def toggle_color(row_frame):
-    bg_color = row_frame.cget("bg")
-    if bg_color == "red":
-        row_frame.configure(bg="green")
-        row_frame.winfo_children()[0].configure(bg="green", fg="white")
-        row_frame.winfo_children()[1].configure(bg="green")
-    else:
-        row_frame.configure(bg="red")
-        row_frame.winfo_children()[0].configure(bg="red", fg="white")
-        row_frame.winfo_children()[1].configure(bg="red")
-
 
 def upload_file():
     # Open a GUI window to browse for a file
@@ -78,8 +94,8 @@ def upload_file():
             messagebox.showerror("Error", "Selected file is not a valid JSON file.")
             return
 
-    # Show the identifiers in a pop-up window
-    show_identifiers(data)
+    # instantiate statuses array, will be stored as array of pairs (index, status)
+    statuses = []
 
     # Check if db has a document with the same identifier
     # If it does, ask the user if they want to override it
@@ -92,22 +108,23 @@ def upload_file():
 
         if existing_doc:
             # If the document exists, ask the user if they want to keep the original or override
-            confirm_override = messagebox.askyesno("Document already exists", f"A document with identifier {doc_id} already exists in the database. Do you want to override it?")
-
-            if confirm_override:
-                # If the user confirmed the override, update the existing document with the new information
-                collection.update_one({"_id": existing_doc["_id"]}, {"$set": molecule})
-                messagebox.showinfo("Success", f"The document with identifier {doc_id} has been updated.")
+            if set(molecule.keys()) == set(existing_doc.keys()):
+                # If the keys are the same, check if the values are the same
+                if molecule == existing_doc:
+                    # If the values are the same, do nothing
+                    statuses.insert(0, (molecule, Status.CONFLICT)) # push front
+                    continue
             else:
-                # If the user did not confirm the override, do nothing
-                return
-
+                statuses.append((molecule, Status.WARNING))
         else:
-            print(doc_id)
             # If the document does not exist, insert it into the database
             molecule["identifier"] = doc_id
             collection.insert_one(molecule)
-            messagebox.showinfo("Success", f"The document with identifier {doc_id} has been added to the database.")
+            statuses.append((molecule, Status.SUCCESS))
+
+        
+    # Show the identifiers in a pop-up window
+    show_identifiers(statuses)
 
 if __name__ == "__main__":
     upload_file()
